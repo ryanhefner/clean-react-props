d23e8b61272cc7cffe49e33de141d3ea6d92d18c
Ryan Hefner <hi@ryanhefner.com>
2018-07-13 11:03:46 -0400

    Wrap tests in describe and add test for excluding SVG props

--- src/index.test.js
+++ src/index.test.js
@@ -1,121 +1,138 @@
 import cleanProps, { cleanSVGProps } from './index';
 import { HTMLProps, RDFProps, SVGProps, ValidEvents } from './constants';
 
-test('confirm only valid props are returned', () => {
-  const cleanedProps = cleanProps({
-    something: '',
-    className: '',
+describe('clean-react-props', () => {
+  test('confirm only valid props are returned', () => {
+    const cleanedProps = cleanProps({
+      something: '',
+      className: '',
+    });
+
+    const evalProps = {
+      className: '',
+    };
+
+    expect(cleanedProps).toEqual(evalProps);
   });
 
-  const evalProps = {
-    className: '',
-  };
+  test('confirm that data- attributes are retained', () => {
+    const cleanedProps = cleanProps({
+      'something': '',
+      'data-rocks': '🤘',
+    });
 
-  expect(cleanedProps).toEqual(evalProps);
-});
+    const evalProps = {
+      'data-rocks': '🤘',
+    };
 
-test('confirm that data- attributes are retained', () => {
-  const cleanedProps = cleanProps({
-    'something': '',
-    'data-rocks': '🤘',
+    expect(cleanedProps).toEqual(evalProps);
   });
 
-  const evalProps = {
-    'data-rocks': '🤘',
-  };
+  test('confirm that aria- attributes are retained', () => {
+    const cleanedProps = cleanProps({
+      'aria-label': 'read me',
+      'something': '',
+    });
 
-  expect(cleanedProps).toEqual(evalProps);
-});
+    const evalProps = {
+      'aria-label': 'read me',
+    };
 
-test('confirm that aria- attributes are retained', () => {
-  const cleanedProps = cleanProps({
-    'aria-label': 'read me',
-    'something': '',
+    expect(cleanedProps).toEqual(evalProps);
   });
 
-  const evalProps = {
-    'aria-label': 'read me',
-  };
+  test('confirm that HTMLProps are retained', () => {
+    const htmlProps = {};
 
-  expect(cleanedProps).toEqual(evalProps);
-});
+    HTMLProps.forEach(key => htmlProps[key] = '');
 
-test('confirm that HTMLProps are retained', () => {
-  const htmlProps = {};
+    const cleanedProps = cleanProps({
+      ...htmlProps,
+      something: '',
+    });
 
-  HTMLProps.forEach(key => htmlProps[key] = '');
-
-  const cleanedProps = cleanProps({
-    ...htmlProps,
-    something: '',
+    expect(cleanedProps).toEqual(htmlProps);
   });
 
-  expect(cleanedProps).toEqual(htmlProps);
-});
+  test('confirm that RDFProps are retained', () => {
+    const rdfProps = {};
 
-test('confirm that RDFProps are retained', () => {
-  const rdfProps = {};
+    RDFProps.forEach(key => rdfProps[key] = '');
 
-  RDFProps.forEach(key => rdfProps[key] = '');
+    const cleanedProps = cleanProps({
+      ...rdfProps,
+      something: '',
+    });
 
-  const cleanedProps = cleanProps({
-    ...rdfProps,
-    something: '',
+    expect(cleanedProps).toEqual(rdfProps);
   });
 
-  expect(cleanedProps).toEqual(rdfProps);
-});
+  test('confirm that SVGProps are retained', () => {
+    const svgProps = {};
 
-test('confirm that SVGProps are retained', () => {
-  const svgProps = {};
+    SVGProps.forEach(key => svgProps[key] = '');
 
-  SVGProps.forEach(key => svgProps[key] = '');
+    const cleanedProps = cleanSVGProps({
+      ...svgProps,
+      something: '',
+    });
 
-  const cleanedProps = cleanSVGProps({
-    ...svgProps,
-    something: '',
+    expect(cleanedProps).toEqual(svgProps);
   });
 
-  expect(cleanedProps).toEqual(svgProps);
-});
+  test('confirm that ValidEvents are retained', () => {
+    const validEvents = {};
 
-test('confirm that ValidEvents are retained', () => {
-  const validEvents = {};
+    ValidEvents.forEach(key => validEvents[key] = () => {});
 
-  ValidEvents.forEach(key => validEvents[key] = () => {});
+    const cleanedProps = cleanProps({
+      ...validEvents,
+      something: '',
+    });
 
-  const cleanedProps = cleanProps({
-    ...validEvents,
-    something: '',
+    expect(cleanedProps).toEqual(validEvents);
   });
 
-  expect(cleanedProps).toEqual(validEvents);
-});
+  test('confirm that specified props are excluded', () => {
+    const cleanedProps = cleanProps({
+      'className': '',
+      'width': '',
+      'something': '',
+    }, ['width']);
 
-test('confirm that specified props are excluded', () => {
-  const cleanedProps = cleanProps({
-    'className': '',
-    'width': '',
-    'something': '',
-  }, ['width']);
+    const evalProps = {
+      'className': '',
+    };
 
-  const evalProps = {
-    'className': '',
-  };
+    expect(cleanedProps).toEqual(evalProps);
+  });
 
-  expect(cleanedProps).toEqual(evalProps);
-});
+  test('confirm that specified customAttributes are retained', () => {
+    const cleanedProps = cleanProps({
+      'className': '',
+      'something': '',
+    }, [], ['something']);
 
-test('confirm that specified customAttributes are retained', () => {
-  const cleanedProps = cleanProps({
-    'className': '',
-    'something': '',
-  }, [], ['something']);
+    const evalProps = {
+      'className': '',
+      'something': '',
+    };
 
-  const evalProps = {
-    'className': '',
-    'something': '',
-  };
+    expect(cleanedProps).toEqual(evalProps);
+  });
 
-  expect(cleanedProps).toEqual(evalProps);
+  test('confirm props are excluded - SVGProps', () => {
+    const cleanedProps = cleanSVGProps({
+      'accentHeight': '',
+      'clip': '',
+      'calcMode': '',
+    }, ['calcMode']);
+
+    const evalProps = {
+      'accentHeight': '',
+      'clip': '',
+    };
+
+    expect(cleanedProps).toEqual(evalProps);
+  });
 });
